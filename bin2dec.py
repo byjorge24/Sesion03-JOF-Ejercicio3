7ff66f12bcf0ca0e8d8fe3699971602b9ed8646b
Rich-Hary <113539811+Rich-Hary@users.noreply.github.com>
2022-09-28 18:38:24 +0200

    Update bin2dec.py

--- bin2dec.py
+++ bin2dec.py
@@ -3,7 +3,7 @@
 # Convierte un número binario a decimal.
 # El binario es un string e.g. "101"
 # ------------------------------------------------------
-def bin2dec(numero_binario):
+def bin2dec(numero_binario):.
     numero_decimal = 0
 
     j = len(numero_binario) - 1
@@ -29,3 +29,5 @@ if __name__ == "__main__":
     # Muestra por pantalla el resultado.
     # Para imprimir un entero es necesario convertirlo a string con str()
     print("El numero binario " + numero_binario + " es " + str(numero_decimal) + " en decimal.")
+
+    
